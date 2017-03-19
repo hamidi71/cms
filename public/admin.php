14eca7059451e817521c7068ed472388fdc7c30d
hamidi71 <chiahna71@gmail.com>
2017-03-19 23:12:04 +0100

    Initial commit

---
+++ public/admin.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../includes/connectdb.php';
+include '../includes/layout/header.php';
+include '../includes/functions.php';
+?>
+<body>
+
+<div class="container theme-showcase" role="main">
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h1>Admin Area</h1>
+        <p>Welkom To CMS Baddi Hamidi Admin</p>
+    </div>
+</div>
+<?php
+
+include '../includes/layout/footer.php';
+?>
+
+</body>
+</html>
